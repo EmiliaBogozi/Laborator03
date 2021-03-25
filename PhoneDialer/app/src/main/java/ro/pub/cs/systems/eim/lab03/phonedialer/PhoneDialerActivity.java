aa9a25dffb639d7b980f3a956edc1a7aa2a90e3c
Emilia Bogozi <73529202+EmiliaBogozi@users.noreply.github.com>
2021-03-25 18:57:21 +0200

    implemented taks for laboratory 03

--- PhoneDialer/app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/PhoneDialerActivity.java
+++ PhoneDialer/app/src/main/java/ro/pub/cs/systems/eim/lab03/phonedialer/PhoneDialerActivity.java
@@ -6,6 +6,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.media.Image;
 import android.os.Bundle;
@@ -26,6 +27,7 @@ public class PhoneDialerActivity extends AppCompatActivity {
     private ButtonClickListener buttonClickListener = new ButtonClickListener();
     private static final int PERMISSION_REQUEST_CALL_PHONE = 0;
 
+    //Vector butoane tipul Button
     public int[] buttonsId = {
             R.id.button_1,
             R.id.button_2,
@@ -40,12 +42,14 @@ public class PhoneDialerActivity extends AppCompatActivity {
             R.id.button_0,
             R.id.button_hashtag
     };
+    //Vector butoane tipul ImageButton
     public int[] imageButtonsId = {
             R.id.imageButton_backspace,
             R.id.imageButton_call,
             R.id.imageButton_endcall
     };
 
+    //TODO: Să se implementeze o clasă ascultător pentru tratarea evenimentelor de tip apăsare de buton
     private class ButtonClickListener implements View.OnClickListener {
 
         @Override
@@ -53,16 +57,21 @@ public class PhoneDialerActivity extends AppCompatActivity {
             EditText phoneNumberEditText = (EditText)findViewById(R.id.editTextPhone);
             String phoneNumber = phoneNumberEditText.getText().toString();
 
+            //TODO: Pentru butoanele ce conțin cifre sau caracterele * / #, se va adăuga simbolul corespunzător la numărul de telefon care se dorește format
             if(view instanceof Button) {
                 phoneNumberEditText.setText(phoneNumber+((Button)view).getText().toString());
             }
 
             if(view instanceof ImageButton) {
+                //TODO: Pentru butonul de corecție, se va șterge ultimul caracter (în cazul în care numărul de telefon nu este vid);
                 if(view.getId() == R.id.imageButton_backspace) {
                     if (phoneNumber.length() > 0) {
                         phoneNumberEditText.setText(phoneNumber.substring(0, phoneNumber.length() - 1));
                     }
                 }
+                //TODO: pentru butonul de apel, se va invoca intenția care realizează legătura telefonică;
+                // întrucât se compilează proiectul Android folosind o versiune mai mare decât Marshmelow (6.0),
+                // este necesar să fie solicitată permisiunea de efectuare a apelului telefonic la momentul rulării:
                 if(view.getId() == R.id.imageButton_call) {
                     if (ContextCompat.checkSelfPermission(PhoneDialerActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                         ActivityCompat.requestPermissions(PhoneDialerActivity.this,
@@ -74,6 +83,7 @@ public class PhoneDialerActivity extends AppCompatActivity {
                         startActivity(intent);
                     }
                 }
+                //TODO: pentru butonul de oprire, se va închide activitatea
                 if(view.getId() == R.id.imageButton_endcall) {
                     finish();
                 }
@@ -85,6 +95,9 @@ public class PhoneDialerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_dialer);
+
+        //TODO: să se blocheze tranziția între modurile portrait și landscape
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         ArrayList<? super View> phoneButtons = new ArrayList<View>();
         for (int index = 0; index < buttonsId.length; index++) {
